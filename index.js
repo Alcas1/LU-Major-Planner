a2cde98766b40ec9d3a69b25e5e7d61a11e57914
Jonathan Wu <vwl217@lehigh.edu>
2015-11-01 19:44:59 -0500

    added some courses

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 var http = require('http');  
 var express = require('express');
-var mongodb = require('mongodb');
-var MongoClient = mongodb.MongoClient;
-var url = 'mongodb://admin:admin@ds045464.mongolab.com:45464/lumajorplanner';
 var app = express();
-
+var classes=[];
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -15,29 +12,12 @@ app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
 
-
-// Use connect method to connect to the Server
-MongoClient.connect(url, function (err, db) {
-  if (err) {
-    console.log('Unable to connect to the mongoDB server. Error:', err);
-  } else {
-    //HURRAY!! We are connected. :)
-    console.log('Connection established to', url);
-
-    // do some work here with the database.
-
-    //Close connection
-    db.close();
-  }
-});
-
-
 app.get('/', function(request, response) {
-  response.render('index.html');
+	response.render('index.html');
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+	console.log('Node app is running on port', app.get('port'));
 });
 
 
--- public/js/main.controller.js
+++ public/js/main.controller.js
@@ -1,4 +1,5 @@
 
+
 var app = angular.module('LUMajorPlanner', []);
 app.controller('MainController', function ($scope) {
 	$scope.state="schools";
@@ -6,12 +7,31 @@ app.controller('MainController', function ($scope) {
 	{fullname:'Arts and Sciences',img:"/assets/ArtsandScience.jpg"},
 	{fullname:"Business and Economics",img:"/assets/Business.jpg"}];
 	$scope.Schools2=[{fullname:"Education",img:"/assets/Education.jpg"}];
-	$scope.Engineering=[{name:"Computer Science and Engineering"},
-	{name:"Computer Science and Engineering"},
-	{name:"Computer Science and Engineering"},
-	{name:"Computer Science and Engineering"},
-	{name:"Computer Science and Engineering"}];
-	
+	$scope.Engineering=[{name:"Computer Science and Engineering"}];
+
+	function course(name,prereq,coreq,major){
+			this.name = name;
+			this.prereq = prereq;
+			this.coreq = coreq;
+			this.major = major;
+		}
+	var courseHash={};
+	var courses=[
+			new course('CSE 001',[],[],['CSE']),
+    	    new course('CSE 002',[],[],['CSE,ISE']),
+    	    new course('CSE 017',['CSE 001','CSE 002'],[],['CSE,ISE']),
+			new course('CSE 109',['CSE 017'],[],['CSE']),
+			new course('CSE 130',['CSE 017'],[],['CSE']),
+			new course('CSE 202',['CSE 017'],[],['CSE']),
+			new course('CSE 216',['CSE 109'],[],['CSE']),
+			new course('CSE 261',['MATH 021'],[],['CSE']),
+			new course('CSE 262',['CSE 017'],[],['CSE']),
+			
+			new course('CSE 216',['CSE 109'],[],['CSE']),
+			new course('CSE 261',['MATH 021'],[],['CSE']),
+			new course('CSE 262',['CSE 017'],[],['CSE']),
+			
+			];
 
 
 });
